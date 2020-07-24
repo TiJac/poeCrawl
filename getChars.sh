27a0eec12a6a48628125dad4b1e35bc5aed3d65f
Timo Jacobs <itmo@jornle.de>
2020-07-25 00:15:48 +0200

    initial version

---
+++ getChars.sh
@@ -0,0 +1,4 @@
+mkdir -p poeCrawl
+while read p; do
+  curl --cookie "POESESSID=$1" https://www.pathofexile.com/character-window/get-items?character=$p | jq --compact-output '{eins:[.items[].sockets], zwei:[.items[]?.socketedItems]}' | jq --compact-output '.eins as $sockets | .zwei as $skills | reduce range(0; $skills|length) as $i ([]; . + [{ "socketgroups": [$sockets[$i][]?.group], "skills": [$skills[$i][]?.typeLine] }])' > ./poeCrawl/$p.skills.json
+done 
